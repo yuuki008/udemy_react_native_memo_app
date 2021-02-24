c543a81632d5331044f3df3b54b27e3e9d8e0a2a
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-02-24 17:32:27 +0900

    add: コンポーネント読み込み

--- App.js
+++ App.js
@@ -7,12 +7,12 @@ import {
 } from "@react-navigation/stack";
 import firebase from "firebase";
 
-// import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
-// import MemoCreateScreen from './src/screens/MemoCreateScreen';
-// import LogInScreen from './src/screens/LogInScreen';
-// import SignUpScreen from './src/screens/SignUpScreen';
+import MemoListScreen from "./src/screens/MemoListScreen";
+import MemoDetailScreen from "./src/screens/MemoDetailScreen";
+import MemoEditScreen from "./src/screens/MemoEditScreen";
+import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+import LogInScreen from "./src/screens/LogInScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
 
 import { firebaseConfig } from "./env";
 
